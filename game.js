ed5f9e703307670fb61766c134ea1405abd036fe
AbhirajGOAT <84024511+AbhirajGOAT@users.noreply.github.com>
2022-04-10 17:08:59 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,12 @@
+function weight_age(){
+    window.location="https://studio.code.org/projects/applab/J00zdyz9v0L_63vMhfkuze4vHxQ3eG9ivvGMWgxUkCE";
+}
+function guess_color(){
+    window.location="https://studio.code.org/projects/applab/KnKCP-mGs09qhhtl1F8DoabFJpN_E0215J0uwnSxu9Y";
+}
+function roll_dice(){
+    window.location="https://studio.code.org/projects/applab/DYIHYT3s3HqNcXEJdLPywrzw5pnpjm4OoLnArRWq4eY";
+}
+function avengers_quiz(){
+window.location="https://studio.code.org/projects/applab/DN0Uowybrb5l0-B6C52dtEamrtpb48Mx75DLm7WQSSY";
+}
